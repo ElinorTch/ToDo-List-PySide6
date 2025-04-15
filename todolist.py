c255d5889b420d319a0f45d14f2233e83c704934
ElinorTch <tchamoeliicode@gmail.com>
2025-04-15 12:26:40 +0200

    Feat: database integrated

--- todolist.py
+++ todolist.py
@@ -1,8 +1,12 @@
 import sys
-import random
 from PySide6 import QtCore
 from PySide6.QtWidgets import (QListWidget, QListWidgetItem, QApplication, QDialog, QGroupBox,QLabel, QLineEdit, QPushButton, QVBoxLayout)
+import sqlite3
 
+
+con = sqlite3.connect("todolist.db")
+cur = con.cursor()
+tasks = cur.execute("SELECT description FROM task").fetchall()
 
 class MyWidget(QDialog):
 
@@ -10,11 +14,13 @@ class MyWidget(QDialog):
         super().__init__()
 
         self.setWindowTitle("ToDo-List")
-        # self.create_tasks_group_box()
         
         self.edit = QLineEdit("")
         button = QPushButton("Tout supprimer")
+
         self.tasks_list = QListWidget()
+        for task in tasks:
+            self.tasks_list.addItem(f"{task[0]}")
         self.tasks_list.itemDoubleClicked.connect(self.delete_task)
 
         button.setAutoDefault(False)
@@ -30,15 +36,21 @@ class MyWidget(QDialog):
 
     @QtCore.Slot()
     def add_task(self):
+        cur.execute(f"INSERT INTO task('description') VALUES ('{self.edit.text()}')")
+        con.commit()
         self.tasks_list.addItem(f"{self.edit.text()}")
         self.edit.clear()
 
     def delete_task(self, item):
         self.tasks_list.takeItem(self.tasks_list.row(item))  
+        cur.execute(f"DELETE FROM task WHERE description='{item.text()}'")
+        con.commit()
         
     @QtCore.Slot()
     def delete_tasks(self):
         self.tasks_list.clear()
+        cur.execute("DELETE FROM task;")
+        con.commit()
 
 
 if __name__ == "__main__":
