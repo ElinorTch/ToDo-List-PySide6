98b5d83dd60a08d7f533f095de79e4fb68c902a4
ElinorTch <tchamoeliicode@gmail.com>
2025-04-22 08:32:56 +0200

    Feat: Implementation Must instanciate all the method of the interface before uses

--- logic/category_repository.py
+++ logic/category_repository.py
@@ -1,6 +1,18 @@
-class CategoryRepository():
-    def insert(self, label): pass
+from abc import ABC, abstractmethod
+
+
+class CategoryRepository(ABC):
+    @abstractmethod
+    def insert(self, label) -> None: pass
+
+    @abstractmethod
     def delete_one(self, label): pass
+    
+    @abstractmethod
     def delete_all(self): pass
+
+    @abstractmethod
     def getAll(self): pass
+    
+    @abstractmethod
     def getOne(self, label): pass
